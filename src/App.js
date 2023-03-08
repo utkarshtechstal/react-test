f80f7440855b47f9667b3de5adb3146456450e18
abbyutk <abhaynabby@gmail.com>
2023-03-09 01:51:20 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Redirect, Switch, BrowserRouter, Router } from "react-router-dom";
+import signIn from "./Pages/SignIn";
+import ProfilePage from "./Pages/profile";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+
+const App = () => {
+
+    return (
+            <BrowserRouter>
+                <Switch>
+                  <Redirect exact from="/" to="/login" />
+                  <Route
+                      exact
+                      path="/login"
+                      component={signIn}
+                  />
+                  <Route
+                      exact
+                      path="/profile"
+                      component={ProfilePage}
+                  />
+                  {/* <Route exact path="/not-found" component={NotFound} /> */}
+                  {/* <Redirect to={`not-found`} /> */}
+                </Switch>
+            </BrowserRouter>
+    );
+};
+
 
 export default App;
